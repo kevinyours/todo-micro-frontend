6ac3e1562536da42f8c890ad7d54a2d5c5cbc420
kevin-grylls <eilgwon88@gmail.com>
2022-02-10 12:46:43 +0900

    feat: final

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,6 +15,8 @@ const prodConfig = {
       name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        auth: `auth@${domain}/auth/remoteEntry.js`,
+        dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
